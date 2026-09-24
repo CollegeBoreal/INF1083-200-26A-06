696a7cbf78d4dba60ce10606a84481821b59f856
B300098957 <300098957@collegeboreal.ca>
2026-09-23 22:54:18 -0400

    .

--- 2.NativeScript/B300098957/nativescript.config.ts
+++ 2.NativeScript/B300098957/nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.BonjourMonde',
+  id: 'org.nativescript.B300098957',
   appPath: 'src',
   appResourcesPath: 'App_Resources',
   android: {
